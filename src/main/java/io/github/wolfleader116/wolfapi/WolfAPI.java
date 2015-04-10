2561656912bfa0a4f200b619221fef958bd57e9f
WolfLeader116 <kaio11602@gmail.com>
2015-04-10 08:52:23 -0700

    Update 1.0

--- src/main/java/io/github/wolfleader116/wolfapi/WolfAPI.java
+++ src/main/java/io/github/wolfleader116/wolfapi/WolfAPI.java
@@ -1,5 +1,9 @@
 package io.github.wolfleader116.wolfapi;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,8 +16,48 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class WolfAPI {
+public class WolfAPI extends JavaPlugin {
+
+	public static WolfAPI plugin;
+	
+	@Override
+	public void onEnable() {
+		File db = new File(getDataFolder(), "itemdatabase.yml");
+		if(!db.exists()) {
+			db.getParentFile().mkdirs();
+			copy(getResource("itemdatabase.yml"), db);
+		} else {
+			Config dab = new Config("itemdatabase", this);
+			if (dab.getConfig().getInt("Version") != 1) {
+				db.delete();
+				db.getParentFile().mkdirs();
+				copy(getResource("itemdatabase.yml"), db);
+			}
+		}
+		plugin = this;
+	}
+	
+	@Override
+	public void onDisable() {
+		plugin = null;
+	}
+
+	private void copy(InputStream in, File file) {
+		try {
+			OutputStream out = new FileOutputStream(file);
+			byte[] buf = new byte[1024];
+			int len;
+			while((len=in.read(buf))>0) {
+				out.write(buf,0,len);
+			}
+			out.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void particle(EnumParticle particle, boolean boo, float xcent, float ycent, float zcent, int xoff, int yoff, int zoff, int speed, int amount, Player player) {
 		PacketPlayOutWorldParticles particlepacket = new PacketPlayOutWorldParticles(particle, boo, xcent, ycent, zcent, xoff, yoff, zoff, speed, amount);
@@ -35,6 +79,12 @@ public class WolfAPI {
 			item.addUnsafeEnchantment(enchdata, Integer.parseInt(enchinfo[1]));
 		}
 		return item;
+	}
+	
+	public static String getItemName(String input) {
+		Config dab = new Config("itemdatabase", WolfAPI.plugin);
+		String output = dab.getConfig().getString("Items." + input);
+		return output;
 	}
 	
 }
