01110ec418a32c3f28795cc1260cc9df854cc0d2
WolfLeader116 <kaio11602@gmail.com>
2015-05-03 09:38:51 -0700

    Update 1.0

--- src/main/java/io/github/wolfleader116/wolfapi/Errors.java
+++ src/main/java/io/github/wolfleader116/wolfapi/Errors.java
@@ -4,7 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-public enum Error {
+public enum Errors {
 
 	NO_PERMISSION,
 	NOT_ONLINE,
@@ -12,21 +12,21 @@ public enum Error {
 	NOT_NUMBER,
 	DEFAULT;
 	
-	public static void sendError(Error error, Player p, String prefix) {
+	public static void sendError(Errors error, Player p, String prefix) {
 		Location loc = p.getLocation();
-		if (error == Error.NO_PERMISSION) {
+		if (error == Errors.NO_PERMISSION) {
 			Particles.FIREWORKS_SPARK.display((float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5, loc, p);
 			p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "You do not have permission to do this!");
-		} else if (error == Error.NOT_ONLINE) {
+		} else if (error == Errors.NOT_ONLINE) {
 			Particles.FIREWORKS_SPARK.display((float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5, loc, p);
 			p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That player is not online!");
-		} else if (error == Error.NEVER_JOINED) {
+		} else if (error == Errors.NEVER_JOINED) {
 			Particles.FIREWORKS_SPARK.display((float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5, loc, p);
 			p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That player has never joined the server!");
-		} else if (error == Error.NOT_NUMBER) {
+		} else if (error == Errors.NOT_NUMBER) {
 			Particles.FIREWORKS_SPARK.display((float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5, loc, p);
 			p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That is not a number!");
-		} else if (error == Error.DEFAULT) {
+		} else if (error == Errors.DEFAULT) {
 			Particles.FIREWORKS_SPARK.display((float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5, loc, p);
 			p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "There has been an error! Please contact server administrators.");
 		}
