2cd4ef3231284a77173b61fd10afd33a8e376e3a
WolfLeader116 <kaio11602@gmail.com>
2015-10-03 19:43:13 -0700

    Update Pre 3.0-1

--- src/main/java/io/github/wolfleader116/wolfapi/Errors.java
+++ src/main/java/io/github/wolfleader116/wolfapi/Errors.java
@@ -10,7 +10,8 @@ public enum Errors {
 	NOT_ONLINE,
 	NEVER_JOINED,
 	NOT_NUMBER,
-	DEFAULT;
+	DEFAULT,
+	CUSTOM;
 	
 	public static void sendError(Errors error, Player p, String prefix) {
 		Location loc = p.getLocation();
@@ -30,6 +31,33 @@ public enum Errors {
 			} else if (error == Errors.DEFAULT) {
 				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
 				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "There has been an error! Please contact server administrators.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void sendError(Errors error, Player p, String prefix, String message) {
+		Location loc = p.getLocation();
+		try {
+			if (error == Errors.NO_PERMISSION) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "You do not have permission to do this!");
+			} else if (error == Errors.NOT_ONLINE) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That player is not online!");
+			} else if (error == Errors.NEVER_JOINED) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That player has never joined the server!");
+			} else if (error == Errors.NOT_NUMBER) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "That is not a number!");
+			} else if (error == Errors.DEFAULT) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + "There has been an error! Please contact server administrators.");
+			} else if (error == Errors.CUSTOM) {
+				ParticleEffect.FIREWORKS_SPARK.sendToPlayer(p, loc, (float) 0.8, (float) 0.8, (float) 0.8, (float) 0, 5);
+				p.sendMessage(ChatColor.BLUE + ChatColor.stripColor(prefix) + ChatColor.BLUE + "> " + ChatColor.RED + message);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
