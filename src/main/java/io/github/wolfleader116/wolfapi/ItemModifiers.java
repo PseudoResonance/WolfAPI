5ecd6b58d08ca832a924fcf8e72aa6464f0f4b62
WolfLeader116 <kaio11602@gmail.com>
2015-10-04 13:02:38 -0700

    Update Pre-3.0-3

--- src/main/java/io/github/wolfleader116/wolfapi/ItemModifiers.java
+++ src/main/java/io/github/wolfleader116/wolfapi/ItemModifiers.java
@@ -10,7 +10,7 @@ public class ItemModifiers {
 	public static ItemStack setSoulbound(ItemStack item) {
 		ItemMeta meta = item.getItemMeta();
 		List<String> lores = meta.getLore();
-		lores.add(null);
+		lores.add(" ");
 		lores.add("§5§nSoulbound §f- §aCannot be dropped upon death.");
 		meta.setLore(lores);
 		item.setItemMeta(meta);
@@ -20,7 +20,7 @@ public class ItemModifiers {
 	public static ItemStack setFinal(ItemStack item) {
 		ItemMeta meta = item.getItemMeta();
 		List<String> lores = meta.getLore();
-		lores.add(null);
+		lores.add(" ");
 		lores.add("§5§nFinal §f- §aCannot be modified or repaired.");
 		meta.setLore(lores);
 		item.setItemMeta(meta);
@@ -30,7 +30,7 @@ public class ItemModifiers {
 	public static ItemStack setUnbreakable(ItemStack item) {
 		ItemMeta meta = item.getItemMeta();
 		List<String> lores = meta.getLore();
-		lores.add(null);
+		lores.add(" ");
 		lores.add("§5§nUnbreakable §f- §aCannot be broken.");
 		meta.setLore(lores);
 		item.setItemMeta(meta);
