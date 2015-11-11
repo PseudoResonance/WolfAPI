0a2a6bf099c819f7575fc60b430d4b24eee7cb7a
WolfLeader116 <kaio11602@gmail.com>
2015-11-10 17:29:13 -0800

    Update Pre-3.0-16

--- src/main/java/io/github/wolfleader116/wolfapi/Message.java
+++ src/main/java/io/github/wolfleader116/wolfapi/Message.java
@@ -1,5 +1,7 @@
 package io.github.wolfleader116.wolfapi;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -25,6 +27,38 @@ public class Message {
 	public static void broadcastJSONMessage(ChatElement... elements) {
 		String end = "[\"\"";
 		if (elements.length == 0) {
+			end = end + "]";
+		} else {
+			for (ChatElement e : elements) {
+				end = end + e.build();
+			}
+			end = end + "]";
+		}
+		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "tellraw " + p.getName() + " " + end);
+		}
+	}
+	
+	public static void sendJSONMessage(Player p, List<ChatElement> elements) throws NullPointerException {
+		if (p != null) {
+			String end = "[\"\"";
+			if (elements.size() == 0) {
+				end = end + "]";
+			} else {
+				for (ChatElement e : elements) {
+					end = end + e.build();
+				}
+				end = end + "]";
+			}
+			Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "tellraw " + p.getName() + " " + end);
+		} else {
+			throw new NullPointerException();
+		}
+	}
+	
+	public static void broadcastJSONMessage(List<ChatElement> elements) {
+		String end = "[\"\"";
+		if (elements.size() == 0) {
 			end = end + "]";
 		} else {
 			for (ChatElement e : elements) {
